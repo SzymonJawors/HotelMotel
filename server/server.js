6141130b2c67c44c631d6c2da22c00dce3e0671a
SzymonJawors <jawor12325@gmail.com>
2025-11-19 14:33:33 +0100

    use

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
 
-app.post("/api/clerk", clerkWebhooks);
+app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API DZIAŁA"));
 
